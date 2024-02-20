5d32207e98e913540007b9004140d1f69c9b0c2a
alexandre-eymael <eymael.alex@gmail.com>
2024-02-20 19:35:02 +0100

    Fix typo in frontend

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,8 +1,13 @@
 # Stop existing container
+echo "[1/3] Stopping existing container..."
 sudo docker stop $(sudo docker ps -a -q)
 
 # Build new image
+echo "[2/3] Building new image..."
 bash build_docker.sh
 
 # Start new container
+echo "[3/3] Starting new container..."
 bash start_dockerized_server.sh
+
+echo "DEPLOYMENT COMPLETED!"
